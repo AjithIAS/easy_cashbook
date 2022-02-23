4e1129edbdd582e9c4c115475f960cb92fa7506b
AjithIAS <ajithking7598@gmail.com>
2022-02-23 16:18:33 +0000

    login ui completed

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService
+ } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(public authService : AuthService) { }
 
   ngOnInit() {
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
     /* Saving user data in localstorage when 
     logged in and setting up null when logged out */
     this.afAuth.authState.subscribe(user => {
+      console.log(user)
       if (user) {
         this.userData = user;
         localStorage.setItem('user', JSON.stringify(this.userData));
